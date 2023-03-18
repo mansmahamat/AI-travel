ab9ab2b5ea11cb25b5a4af647d23c35d78880bb9
mansmahamat <mansour.mahamat.salle@gmail.com>
2023-03-18 09:29:47 +0100

    n

--- pages/index.tsx
+++ pages/index.tsx
@@ -602,7 +602,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex-1 relative z-0 flex ">
-            <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
+            <main className="flex-1 relative z-0 flex-shrink-0 overflow-y-auto focus:outline-none xl:order-last">
               {/* Start main area*/}
               <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
                 <div className="h-full   rounded-lg">
@@ -613,7 +613,7 @@ export default function Home() {
               </div>
               {/* End main area */}
             </main>
-            <aside className="hidden relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r  ">
+            <aside className=" relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r  ">
               {/* Start secondary column (hidden on smaller screens) */}
 
               <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
